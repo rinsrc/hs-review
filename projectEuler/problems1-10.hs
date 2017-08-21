e285b3a74149ce967c71aa840672997932e86009
rinsrc <bsrcog@gmail.com>
2017-08-20 17:15:31 -0700

    added more problems

--- projectEuler/problems1-10.hs
+++ projectEuler/problems1-10.hs
@@ -34,3 +34,38 @@ prob4 = maximum $ filter isPalindrome (f [100..999] [100..999] [])
         isPalindrome n = reverse (show n) == (show n)
         f [] _ zs = zs
         f (x:xs) (ys) zs = f xs ys (zs ++ map (*x) ys)
+
+-- (5) smallest multiple
+prob5 :: Integer
+-- foldr1 takes 2 elements from list (start from end) and applies function to it
+prob5 = foldr1 lcm [1..20]
+
+-- (6) sum square difference
+prob6 :: Integer
+prob6 = sum [1..100]^2 - (sum . map (^2)) [1..100]
+
+-- (7) 10001st prime number
+prob7 :: Integer
+prob7 = primes !! 10000 -- primes from prob3 (10000, b/c index starts at 0)
+
+-- (9) special pythagorean triplet
+prob9 :: Integer
+prob9 = (round . product . head) $ filter kSum $ filter isInt $ f 0 [1..500] [2..501] []
+    where
+        -- kSum checks if sum of pythagorean triplet equals 1000
+        kSum xs = if sum xs == 1000 then True else False
+
+        -- isInt checks if c is a an integer or whole number
+        isInt [a,b,c] = if c == fromIntegral (floor c) then True else False
+
+        -- f generates list of lists of potential pythagorean triplets
+        f _ [] _ triplets = triplets
+        f 500 (a:as) bs triplets = f 0 as bs triplets
+        f counter (a:as) bs triplets
+            | (bs !! counter) < a = f (counter+1) (a:as) bs triplets -- prevents duplicate triplets
+            | otherwise = f (counter+1) (a:as) bs ([a,(bs !! counter),c] : triplets)
+                where c = sqrt $ a^2 + (bs !! counter)^2
+
+-- (10) summation of primes
+prob10 :: Integer
+prob10 = sum $ takeWhile (<2000000) primes
