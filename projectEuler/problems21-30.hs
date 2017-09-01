e2261f9b8ec720af101580241b3eceb13de49305
rinsrc <bsrcog@gmail.com>
2017-08-31 19:27:26 -0700

    added problems 28-30

--- projectEuler/problems21-30.hs
+++ projectEuler/problems21-30.hs
@@ -2,7 +2,7 @@ import Data.List
 import System.IO
 import qualified Data.Map as Map
 import Data.Array
-import Data.Char
+import Data.Char (digitToInt)
 import Data.Maybe
 
 -- (21) amicable numbers
@@ -47,19 +47,60 @@ prob22 = do
 
 -- (23) non-abundant sums
 {-
-    divisors n = f n [1..n-1]
-        where
-            f _ [] = []
-            f n (x:xs)
-                | n `mod` x == 0 = x : f n xs
-                | otherwise = f n xs
+divisors n = f n [1..n-1]
+    where
+        f _ [] = []
+        f n (x:xs)
+            | n `mod` x == 0 = x : f n xs
+            | otherwise = f n xs
 
-    abundant n = if n < (sum $ divisors n) then True else False
+abundant n = if n < (sum $ divisors n) then True else False
 
-    f = filter abundant [1..28123]
+f = filter abundant [1..28123]
 -}
 
 -- (25) 1000 digit fibonacci number
 prob25 :: Maybe Int
 prob25 = elemIndex 1000 $ takeWhile (<1001) $ map (length . show) fibs
     where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+-- (28) number spiral diagonals
+-- some observations:
+-- gridSize is always incremented by 2: [1,3,5,7,9..]
+-- 4 diagonal numbers (diagNum) per gridSize (except 1): gridSize 3 has numbers [3,5,7,9], gridSize 5 has numbers [13,17,21,25]
+-- last number in a gridSize is (gridSize^2): 3^2 = 9, 5^2 = 25, 7^2 = 49, ...
+-- diagNum sequence is computed by (gridSize-1): gridSize 3 = ((3-1) = +2) [3,5,7,9], gridSize 5 = ((5-1) = +4) [13,17,21,25], ...
+prob28 :: Integer
+prob28 = sum $ f 1001 (1001^2) 0
+    where
+        -- f works backwards by finding 4 diagNums per gridSize
+        -- starting from 1001 x 1001 gridSize, then 999 x 999 gridSize, ...
+        f gridSize diagNum counter
+            | gridSize == 1 = [1] -- if gridSize is 1 (center of grid), we found all diagonal numbers
+            | counter == 4 = f (gridSize-2) diagNum 0 -- when counter is 4, reset to 0 and decrement grid size by 2
+            | otherwise = diagNum : f gridSize (diagNum-(gridSize-1)) (counter+1)  -- calculate the 4 numbers for current gridSize
+
+-- (29) distinct powers
+prob29 :: Int
+prob29 = (length . nub . concat) $ f [2..100]
+    where
+        -- f makes a list of lists 
+        -- [ [2^2,3^2,,...100^2], [2^2,3^2,..100^2], ..[2^100,3^100,..100^100] ]
+        f [] = []
+        f (x:xs) = map (^x) [2..100] : f xs
+
+-- (30) digit fifth powers
+prob30 :: Int
+prob30 = sum $ filter fifthPowerSum [2..upperBound]
+    where
+        -- upperBound determined by comparing biggest fifthPowerSum to smallest n-digit number
+        -- excluse 1 digit numbers
+        -- 2 digit = 9^5 * 2 > 1, range : [1..9^5*2]
+        -- 3 digit = 9^5 * 3 > 10, range : [1..9^5*3]
+        -- 6 digit = 9^5 * 6 > 100000, range : [1..9^5*6]
+        -- 7 digit = 9^5 * 7 < 1000000
+        -- at 7 digit numbers, the biggest fifthPowerSum (9^5 * 7 = 413343) is less than 1 million (smallest 7-digit number)
+        -- in other words, fifthPowerSum of 7 digit numbers makes only 6 digit numbers (violates rules of problem 30)
+        -- therefore, upperBound is 9^5 * 6 = 354294
+        upperBound = 354294
+        fifthPowerSum n = if (sum . map ((^5) . digitToInt)) (show n) == n then True else False
